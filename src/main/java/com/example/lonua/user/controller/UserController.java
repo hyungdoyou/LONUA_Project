aecc4d7eb46106093986fd7d0da2da7930d0b375
hyungdoyou <yhd4286@gmail.com>
2024-01-12 13:38:57 +0900

    [Docs] 로그인 기능 컨트롤러 추가 컨트롤러 추가

--- src/main/java/com/example/lonua/user/controller/UserController.java
+++ src/main/java/com/example/lonua/user/controller/UserController.java
@@ -2,9 +2,11 @@ package com.example.lonua.user.controller;
 
 import com.example.lonua.user.model.entity.User;
 import com.example.lonua.user.model.entity.request.PostSignUpReq;
+import com.example.lonua.user.model.entity.request.PostUserLoginReq;
 import com.example.lonua.user.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,11 +29,10 @@ public class UserController {
         return ResponseEntity.ok().body("회원가입 성공");
     }
 
-    // 로그인(세션 기반 로그인)
-    @RequestMapping(method = RequestMethod.GET, value = "/mypage")
-    public ResponseEntity mypage() {
-
-        return ResponseEntity.ok().body("마이 페이지입니다.");
+    // 로그인
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
+    public ResponseEntity login(@RequestBody PostUserLoginReq postUserLoginReq) {
+        return ResponseEntity.ok().body(userService.login(postUserLoginReq));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/list")
