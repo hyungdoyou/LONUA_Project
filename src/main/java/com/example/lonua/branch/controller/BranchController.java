5f26293ab514a2269a1c55e133ae37592e9ea569
dk <pivottocoding@gmail.com>
2024-01-13 00:14:55 +0900

    [Feat] 브랜치 Entity Controller 추가 브랜치 Entity Controller 생성

---
+++ src/main/java/com/example/lonua/branch/controller/BranchController.java
@@ -0,0 +1,9 @@
+package com.example.lonua.branch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BranchController {
+}
